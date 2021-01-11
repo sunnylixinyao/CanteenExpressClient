4ee20967dc6472c16abe430c2e3dc3ef3c6c8078
smarteasyx <71005940+smarteasyx@users.noreply.github.com>
2021-01-11 16:06:21 +0800

    2021/1/11 no change

--- app/src/main/java/com/lixinyao/canteenexpressclient/fragment/LogininFragment.java
+++ app/src/main/java/com/lixinyao/canteenexpressclient/fragment/LogininFragment.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,7 +21,6 @@ import com.lixinyao.canteenexpressclient.util.JSONtools;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 
 import okhttp3.Call;
 import okhttp3.Response;
